12f3bd370f9854ceea1d8c3ecc0314ba48377120
Louie Schoenknecht <luisesch97@gmail.com>
2021-01-12 15:18:41 -0800

    add remaining definitions, add JS to toggle definitions under headings
    Co-authored-by: Mikey Kriegel <mikkrieg@gmail.com>

--- js/script.js
+++ js/script.js
@@ -1,7 +1,31 @@
 $(document).ready(function() {
-  $("p").hide();
 
-  $("h2").click(function() {
-    $(".show-def").toggle();
+  $("#heading1").click(function() {
+    $("#def1").toggle();
   });
+
+  $("#heading2").click(function() {
+    $("#def2").toggle();
+  });
+
+  $("#heading3").click(function() {
+    $("#def3").toggle();
+  });
+
+  $("#heading4").click(function() {
+    $("#def4").toggle();
+  });
+
+  $("#heading5").click(function() {
+    $("#def5").toggle();
+  });
+
+  $("#heading6").click(function() {
+    $("#def6").toggle();
+  });
+
+  $("#heading7").click(function() {
+    $("#def7").toggle();
+  });
+
 });
